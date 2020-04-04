4505284039f2bd7c41fb032bb6860da4f8a9640b
Ilya Fralkou <dzmuh@outlook.com>
2020-04-04 05:49:33 +0300

    Базовый тест на добавление новых сущностей. И тест на выполнение SQL запроса.

---
+++ efcore/unit-testing/ef-mixed-utesting/test/EfMixedUtesting.Tests/InMemoryDbTests.cs
@@ -0,0 +1,77 @@
+﻿using EfMixedUtesting.Data.Domain;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace EfMixedUtesting.Tests
+{
+    public class InMemoryDbTests : TestBase
+    {
+        public InMemoryDbTests() { }
+
+        [Fact]
+        public async Task ShouldBeAbleToAddAndGetEntity()
+        {
+            // Подготовка
+            using var context = await GetDbContext();
+            Guid id = Guid.NewGuid();
+            context.Articles.Add(new Article
+            {
+                Id = id,
+                Title = "Article Title",
+                Content = new ArticleContent
+                {
+                    Content = "Article content"
+                }
+            });
+            await context.SaveChangesAsync();
+
+            // Выполняем
+            var data = await context.Articles.ToListAsync();
+
+            // Проверяем
+            Assert.Single(data);
+            Assert.Contains(data, d => d.Id == id);
+            Assert.Contains(data, d => d.Title == "Article Title");
+            Assert.Contains(data, d => d.Content.Content == "Article content");
+        }
+
+        /// <summary>
+        /// При использовании In-Memory DB мы не можем выполнять SQL запросы.
+        /// Этот тест проваливается и не случайно.
+        /// </summary>
+        [Fact]
+        [Trait("DbDependat", "")]
+        public async Task ShouldNotBeAbleToExecuteSql()
+        {
+            // Подготовка
+            using var context = await GetDbContext();
+
+            Guid id = Guid.NewGuid();
+            context.Articles.Add(new Article
+            {
+                Id = id,
+                Title = "Article Title",
+                Content = new ArticleContent
+                {
+                    Content = "Article content"
+                }
+            });
+
+            await context.SaveChangesAsync();
+
+            // Выполняем
+            var result = await context.Articles
+                .FromSqlRaw("select * from Articles")
+                .ToListAsync();
+
+            // Код ниже никогда не будет выполнен.
+            // В коде выше мы получим System.NotImplementedException.
+            Assert.Equal(id, result.First().Id);
+        }
+    }
+}
---
+++ efcore/unit-testing/ef-mixed-utesting/test/EfMixedUtesting.Tests/SqliteTests.cs
@@ -0,0 +1,80 @@
+﻿using EfMixedUtesting.Data.Domain;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace EfMixedUtesting.Tests
+{
+    public class SqliteTests : TestBase
+    {
+        public SqliteTests()
+        {
+            UseSqlite();
+        }
+
+        [Fact]
+        public async Task ShouldBeAbleToAddAndGetEntity()
+        {
+            // Подготовка
+            using var context = await GetDbContext();
+
+            Guid id = Guid.NewGuid();
+            context.Articles.Add(new Article
+            {
+                Id = id,
+                Title = "Article Title",
+                Content = new ArticleContent
+                {
+                    Content = "Article content"
+                }
+            });
+
+            await context.SaveChangesAsync();
+
+            // Выполняем
+            var data = await context.Articles.ToListAsync();
+
+            // Проверяем
+            Assert.Single(data);
+            Assert.Contains(data, d => d.Id == id);
+            Assert.Contains(data, d => d.Title == "Article Title");
+            Assert.Contains(data, d => d.Content.Content == "Article content");
+        }
+
+        /// <summary>
+        /// При использовании SQLite мы можем выполнять SQL запросы.
+        /// </summary>
+        [Fact]
+        [Trait("DbDependat", "")]
+        public async Task ShouldBeAbleToExeuteSql()
+        {
+            // Подготовка
+            using var context = await GetDbContext();
+
+            Guid id = Guid.NewGuid();
+            context.Articles.Add(new Article
+            {
+                Id = id,
+                Title = "Article Title",
+                Content = new ArticleContent
+                {
+                    Content = "Article content"
+                }
+            });
+
+            await context.SaveChangesAsync();
+
+            // Выполняем
+            var result = await context.Articles
+                .FromSqlRaw("select * from Articles")
+                .ToListAsync();
+
+            // Запрос должен получить первую запись и преобразовать byte[] в GUID.
+            Assert.Equal(id, result.First().Id);
+        }
+    }
+}
