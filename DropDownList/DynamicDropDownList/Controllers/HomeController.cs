e0188532e0be49bf88ac372f920d238523c452dd
Ilya Fralkou <dzmuh@outlook.com>
2021-03-06 23:37:17 +0100

    Реализация динамического выпадающего списка - работает. Коряво конечно, но работает.

--- DropDownList/DynamicDropDownList/Controllers/HomeController.cs
+++ DropDownList/DynamicDropDownList/Controllers/HomeController.cs
@@ -1,11 +1,16 @@
-using DynamicDropDownList.Models;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.Extensions.Logging;
+
+using Newtonsoft.Json;
+
+using DynamicDropDownList.Models;
 
 namespace DynamicDropDownList.Controllers
 {
@@ -21,6 +26,28 @@ namespace DynamicDropDownList.Controllers
         public IActionResult Index()
         {
             return View(new ArticleEditViewModel());
+        }
+
+        public string GetProductsForCategory(string id)
+        {
+            // get the products from the repository 
+
+            var products = new List<SelectListItem>();
+
+            if (id == "1")
+            {
+                products.Add(new SelectListItem() { Text = "Introduction to Python", Value = "1" });
+                products.Add(new SelectListItem() { Text = "Python Unit Testing", Value = "2" });
+            }
+            else if (id == "2")
+            {
+                products.Add(new SelectListItem() { Text = "JavaScript testing", Value = "1" });
+                products.Add(new SelectListItem() { Text = "JavaScript Ninja", Value = "2" });
+            }
+
+            //return new JavaScriptSerializer().Serialize(products);
+            //return JsonConvert.SerializeObject(products, Formatting.Indented);
+            return System.Text.Json.JsonSerializer.Serialize(products, new System.Text.Json.JsonSerializerOptions { WriteIndented = true });
         }
 
         public IActionResult Privacy()
